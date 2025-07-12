29ad9a0175753036d177b6d5394346ec6746e1e0
syarif0707 <syarifzafri@gmail.com>
2025-07-12 16:48:56 +0800

    supabase integration

--- lib/services/supabase_service.dart
+++ lib/services/supabase_service.dart
@@ -1,8 +1,8 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseService {
-  static const String _supabaseUrl = 'YOUR_SUPABASE_URL'; // Replace with your URL
-  static const String _supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY'; // Replace with your key
+  static const String _supabaseUrl = 'https://fymykhhwvegsuqpekryy.supabase.co'; // Replace with your URL
+  static const String _supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5bXlraGh3dmVnc3VxcGVrcnl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5NzM2NTMsImV4cCI6MjA2NzU0OTY1M30._B4DsNRBOvj1y5oFFOts40sAM-mTvE0cNioBfcavfS4'; // Replace with your key
 
   static Future<void> initialize() async {
     await Supabase.initialize(
